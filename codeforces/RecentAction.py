d11d92951f89abf5a5ce93e034937f96731d1dd6
Vicfred <vicfredboy@gmail.com>
2019-03-01 21:47:29 -0600

    add default objects for absent responses

--- codeforces/RecentAction.py
+++ codeforces/RecentAction.py
@@ -6,5 +6,5 @@ from dataclasses import dataclass
 @dataclass
 class RecentAction:
     timeSeconds: int
-    blogEntry: BlogEntry = 0  # TODO what should i do here? :s
-    comment: Comment = 0
+    blogEntry: BlogEntry = BlogEntry()  # TODO what should i do here? :s
+    comment: Comment = Comment()
