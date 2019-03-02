625c82c47861b36d7113e33e428e75c05451a51d
Vicfred <vicfredboy@gmail.com>
2019-03-01 22:21:26 -0600

    still confused about what to do in this case

--- codeforces/RecentAction.py
+++ codeforces/RecentAction.py
@@ -6,5 +6,9 @@ from dataclasses import dataclass
 @dataclass
 class RecentAction:
     timeSeconds: int
-    blogEntry: BlogEntry = BlogEntry()  # TODO what should i do here? :s, still needs testing
-    comment: Comment = Comment()
+    blogEntry: BlogEntry# = BlogEntry()  # TODO what should i do here? :s, still needs testing
+    comment: Comment# = Comment()
+
+    def __post_init__(self):
+        self.blogEntry = BlogEntry(**self.blogEntry)
+        self.comment = Comment(**self.comment)
